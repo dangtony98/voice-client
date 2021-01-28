a1f8f98f87bab5cdc38dc4657479305c0f47b687
Tuan Dang <qd38@cornell.edu>
2021-01-28 13:24:37 +0200

    Modify upload modal captioning screen

--- src/components/modals/UploadModal.js
+++ src/components/modals/UploadModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, Platform, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, TextInput, Platform, Dimensions, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import AudioRecorderPlayer, {
   AVEncoderAudioQualityIOSType,
@@ -9,6 +9,9 @@ import AudioRecorderPlayer, {
 } from 'react-native-audio-recorder-player';
 
 export default ({ navigation }) => {  
+  const windowWidth = Dimensions.get('window').width;
+
+  // recorder state
   const [audioRecorderPlayer, setAudioRecorderPlayer] = useState(null);
   const [recordSecs, setRecordSecs] = useState(0);
   const [recordTime, setRecordTime] = useState('00:00:00');
@@ -16,10 +19,12 @@ export default ({ navigation }) => {
   const [currentDurationSec, setCurrentDurationSec] = useState(0);
   const [playTime, setPlayTime] = useState('00:00:00');
   const [duration, setDuration] = useState('00:00:00');
-  const [recordState, setRecordState] = useState('NOT_STARTED'); // NOT_STARTED, STARTED, FINISHED, PLAYING
+  const [recordState, setRecordState] = useState('NOT_STARTED');
+
+  // edit state
+  const [isImageAttached, setIsImageAttached] = useState('');
+  const [caption, setCaption] = useState('');
   const stepRef = useRef(null);
-  const windowWidth = Dimensions.get('window').width;
-  
 
   useEffect(() => {
     setAudioRecorderPlayer(new AudioRecorderPlayer());
@@ -137,7 +142,7 @@ export default ({ navigation }) => {
   }
 
   const onHandleDone = () => {
-    // TO-DO: pan ScrollView
+    handleScroll(windowWidth);
   }
 
   const renderTouchable = (text, onPress, styles) => {
@@ -147,7 +152,7 @@ export default ({ navigation }) => {
           activeOpacity={0.5}
           onPress={() => onPress()}
         >
-          <Text style={[{ fontWeight: '500', fontSize: 20, color: 'rgb(255, 255, 255)'}, styles]}>
+          <Text style={[{ fontWeight: '500', fontSize: 15, color: 'rgb(255, 255, 255)'}, styles]}>
             {text}
           </Text>
         </TouchableOpacity>
@@ -156,18 +161,29 @@ export default ({ navigation }) => {
     return <View />
   }
 
+  const handleScroll = (x) => {
+    stepRef.current.scrollTo({x, y: 0, animated: true})
+  };
+
   return (
     <ScrollView 
       style={{ flex: 1, backgroundColor: 'red' }}
       horizontal={true}
       pagingEnabled={true}
+      scrollEnabled={false}
+      ref={stepRef}
     >
-      <View style={[styles.screen, { width: windowWidth }]}>
-        <View></View>
+      
+      <View style={[styles.recordScreen, { width: windowWidth, backgroundColor: 'rgb(52, 152, 219)' }]}>
+        <View>
+          <Text style={[{ fontWeight: '500', fontSize: 20, color: 'rgb(255, 255, 255)'}, styles]}>
+            Home
+          </Text>
+        </View>
         <Text style={styles.recordTime}>
           {recordTime}
         </Text>
-        <View style={{ flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.recordGroup}>
           {renderTouchable('Reset', onHandleReset, { marginRight: 25 })}
           <TouchableOpacity 
             activeOpacity={0.5}
@@ -181,20 +197,48 @@ export default ({ navigation }) => {
           {renderTouchable('Done', onHandleDone, { marginLeft: 25 })}
         </View>
       </View>
-      <View style={[styles.screen, { width: windowWidth }]}>
-        <View></View>
+      <View style={[styles.editScreen, { width: windowWidth, backgroundColor: 'rgb(255, 255, 255)' }]}>
+        <View style={styles.editTop}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => handleScroll(0)}
+        >
+          <Icon name="chevron-back-outline" size={25} color="rgb(127,140,141)" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => console.log('sdf')}
+        >
+          <Text style={{ fontWeight: '500', color: 'rgb(52,152,219)'}}>Share</Text>
+        </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => console.log('aaa')}
+        >
+          <View style={[styles.editImage, { marginTop: 25 }]}>
+
+          </View>
+        </TouchableOpacity>
+        <TextInput
+          value={caption}
+          onChange={text => setCaption(text)}
+          placeholder="Write a caption..."
+          multiline={true}
+          style={{ flex: 1, marginTop: 15 }}
+        />
       </View>
     </ScrollView>
   ); 
 }
 
 const styles = StyleSheet.create({
-  screen: {
+  recordScreen: {
     paddingVertical: 100,
+    paddingHorizontal: 25,
     flex: 1,
     alignItems: 'center', 
-    justifyContent: 'space-between',
-    backgroundColor: 'rgb(52, 152, 219)'
+    justifyContent: 'space-between'
   },
   recordTime: {
     fontSize: 60,
@@ -221,5 +265,22 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgb(52, 152, 219)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  editScreen: {
+    paddingVertical: 100,
+    paddingHorizontal: 25,
+    flex: 1
+  },
+  editTop: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    width: '100%'
+  },
+  editImage: {
+    height: 350, 
+    width: '100%', 
+    borderRadius: 10,
+    backgroundColor: 'rgba(52, 152, 219, 0.25)'
   }
 });
