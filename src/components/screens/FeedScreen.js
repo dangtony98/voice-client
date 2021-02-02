093da41888815baaa9253c609dcce6f17b920463
Maidul Islam <maidul98@gmail.com>
2021-02-01 20:37:46 -0500

    working auto play

--- src/components/screens/FeedScreen.js
+++ src/components/screens/FeedScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { connect } from 'react-redux';
-import { FlatList, View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {connect} from 'react-redux';
+import {FlatList, View, TouchableOpacity, StyleSheet} from 'react-native';
 import Modal from 'react-native-modal';
 import TextInput from '../generic/TextInput';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -9,42 +9,47 @@ import TrackPlayer from 'react-native-track-player';
 import Audio from '../audio/Audio';
 import AudioBar from '../audio/AudioBar';
 import CommentsModal from '../modals/CommentsModal';
-import { get_feed } from '../../service/api/posts';
-import { useIsFocused } from "@react-navigation/native";
-import { useTrackPlayerProgress } from 'react-native-track-player/lib/hooks';
-import { setFeed, paginateFeed, setCurrentFeed, setCurrentFeedIndex } from '../../actions/feed';
-import { setCurrentTrack } from '../../actions/audio';
-import { createTrack } from '../../service/audio/trackQueue';
+import {get_feed} from '../../service/api/posts';
+import {useIsFocused} from '@react-navigation/native';
+import {useTrackPlayerProgress} from 'react-native-track-player/lib/hooks';
+import {
+  setFeed,
+  paginateFeed,
+  setCurrentFeed,
+  setCurrentFeedIndex,
+} from '../../actions/feed';
+import {setCurrentTrack} from '../../actions/audio';
+import {createTrack} from '../../service/audio/trackQueue';
 
 import store from '../../store/store';
-import { resetPlayer } from '../../actions/audio';
-import { incCurrentFeedIndex } from '../../actions/feed';
+import {resetPlayer} from '../../actions/audio';
+import {incCurrentFeedIndex} from '../../actions/feed';
 
 const POST_HEIGHT = 519;
 
-export const feedScreen = ({ 
+export const feedScreen = ({
   navigation,
   feeds,
   currentFeed,
   setFeed,
   paginateFeed,
-  setCurrentFeed 
+  setCurrentFeed,
 }) => {
-  const [search, setSearch] = useState(""); 
+  const [search, setSearch] = useState('');
   const [isRefreshing, setIsRefreshing] = useState(false);
   const [commentsModalVisible, setCommentsModalVisible] = useState(false);
   const feedRef = useRef(null);
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    get_feed(0, async feedArray => {
+    get_feed(0, async (feedArray) => {
       setFeed(currentFeed, {
         name: currentFeed,
         feed: feedArray,
         skip: feedArray.length,
-        index: 0
+        index: 0,
       });
-      
+
       TrackPlayer.addEventListener('playback-queue-ended', (data) => {
         store.dispatch(resetPlayer());
         store.dispatch(incCurrentFeedIndex());
@@ -53,9 +58,20 @@ export const feedScreen = ({
     });
   }, [isFocused]);
 
-  const renderItem = ({ item, index }) => {
+  const goIndex = () => {
+    feeds = store.getState().feed.feeds;
+    const length = feeds.find((feed) => feed.name == currentFeed).feed.length;
+    const index = feeds.find((feed) => feed.name == currentFeed).index;
+    if (index < length) {
+      const track = feeds.find((feed) => feed.name == currentFeed)?.feed[index];
+      feedRef.current.scrollToIndex({animated: true, index: index});
+      store.dispatch(setCurrentTrack(createTrack(track)));
+    }
+  };
+
+  const renderItem = ({item, index}) => {
     return (
-      <Audio 
+      <Audio
         navigation={navigation}
         item={item}
         {...item}
@@ -63,123 +79,117 @@ export const feedScreen = ({
         setCommentsModalVisible={setCommentsModalVisible}
       />
     );
-  }
+  };
 
-  const snapToNext = () => {
-
-  }
+  const snapToNext = () => {};
 
   const handleMore = () => {
-    const skip = feeds.find(feed => feed.name == currentFeed).skip;
+    const skip = feeds.find((feed) => feed.name == currentFeed).skip;
 
-    get_feed(skip, feedArray => {
+    get_feed(skip, (feedArray) => {
       paginateFeed(currentFeed, feedArray, skip + feedArray.length);
     });
-  }
+  };
 
   const onRefresh = () => {
     setIsRefreshing(true);
-    get_feed(0, async feedArray => {
+    get_feed(0, async (feedArray) => {
       setIsRefreshing(false);
       setFeed(currentFeed, {
         name: currentFeed,
         feed: feedArray,
         skip: feed.length,
-        index: 0
+        index: 0,
       });
       await TrackPlayer.reset();
-    })
-  }
-
-  const goIndex = () => {
-    const index = feeds.find(feed => feed.name == currentFeed).index;
-    // const track = feeds.find(feed => feed.name == currentFeed)?.feed[index];
-    // if (track != undefined) {
-      feedRef.current.scrollToIndex({ animated: true, index: index + 1 });
-      // setCurrentTrack(createTrack(track));
-    // }
+    });
   };
 
   const onNotificationsPressed = () => {
     navigation.navigate('Notifications');
-  }
+  };
 
   return (
-    <View style={{ flex: 1 }}
-    >
+    <View style={{flex: 1}}>
       <View style={styles.navBar}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <TextInput 
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
             value={search}
             placeholder='Try "Cornell"'
             onChangeText={setSearch}
-            otherStyles={{ flex: 1, marginRight: 15 }}
+            otherStyles={{flex: 1, marginRight: 15}}
           />
-          <TouchableOpacity
-            onPress={() => console.log('Nothing yet')}
-          >
-            <Icon name="notifications-outline" size={25} color="rgb(127,140,141)" />
+          <TouchableOpacity onPress={() => console.log('Nothing yet')}>
+            <Icon
+              name="notifications-outline"
+              size={25}
+              color="rgb(127,140,141)"
+            />
           </TouchableOpacity>
         </View>
         <View style={styles.navBox}>
-          <View style={currentFeed == "following" && styles.navSelected}>
+          <View style={currentFeed == 'following' && styles.navSelected}>
             <TouchableOpacityCustom
               title="Following"
-              selected={currentFeed == "following"}
-              onPress={() => setCurrentFeed("following")}
+              selected={currentFeed == 'following'}
+              onPress={() => setCurrentFeed('following')}
             />
           </View>
-          <View style={currentFeed == "trending" && styles.navSelected}>
+          <View style={currentFeed == 'trending' && styles.navSelected}>
             <TouchableOpacityCustom
               title="Trending"
-              selected={currentFeed == "trending"}
-              onPress={() => setCurrentFeed("trending")}
+              selected={currentFeed == 'trending'}
+              onPress={() => setCurrentFeed('trending')}
             />
           </View>
         </View>
       </View>
       <FlatList
-        data={feeds.find(feed => feed.name == currentFeed).feed}
+        data={feeds.find((feed) => feed.name == currentFeed).feed}
         renderItem={renderItem}
-        keyExtractor={item => item._id}
+        keyExtractor={(item) => item._id}
         onEndReached={() => handleMore()}
         onEndReachedThreshold={0.25}
         onRefresh={() => onRefresh()}
         refreshing={isRefreshing}
-        snapToAlignment={"start"}
-        decelerationRate={"fast"}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
         snapToInterval={POST_HEIGHT}
         pagingEnabled
-        getItemLayout={(data, index) => ({ length: POST_HEIGHT, offset: POST_HEIGHT * index, index })}
+        getItemLayout={(data, index) => ({
+          length: POST_HEIGHT,
+          offset: POST_HEIGHT * index,
+          index,
+        })}
         ref={feedRef}
-        style={{ flexGrow: 1 }}
+        style={{flexGrow: 1}}
       />
       <AudioBar />
-      <Modal 
-        isVisible={commentsModalVisible} 
-        style={{ margin: 0, justifyContent: 'flex-end' }}
-        swipeDirection='down'
-        propagateSwipe={true}
-      >
-        <CommentsModal 
-          navigation={navigation} 
+      <Modal
+        isVisible={commentsModalVisible}
+        style={{margin: 0, justifyContent: 'flex-end'}}
+        swipeDirection="down"
+        propagateSwipe={true}>
+        <CommentsModal
+          navigation={navigation}
           setCommentsModalVisible={setCommentsModalVisible}
         />
       </Modal>
-  </View>
+    </View>
   );
-}
+};
 
-const mapStateToProps = ({ feed }) => ({
+const mapStateToProps = ({feed}) => ({
   feeds: feed.feeds,
-  currentFeed: feed.currentFeed
+  currentFeed: feed.currentFeed,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   setFeed: (name, feedObject) => dispatch(setFeed(name, feedObject)),
-  paginateFeed: (name, feedArray, skip) => dispatch(paginateFeed(name, feedArray, skip)),
+  paginateFeed: (name, feedArray, skip) =>
+    dispatch(paginateFeed(name, feedArray, skip)),
   setCurrentFeed: (name) => dispatch(setCurrentFeed(name)),
-  setCurrentTrack: (track) => dispatch(setCurrentTrack(track))
+  setCurrentTrack: (track) => dispatch(setCurrentTrack(track)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(feedScreen);
@@ -191,10 +201,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgb(255, 255, 255)',
   },
   navBox: {
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   navSelected: {
     borderBottomColor: 'rgb(52, 152, 219)',
-    borderBottomWidth: 2
-  }
+    borderBottomWidth: 2,
+  },
 });
--- src/service/audio/trackPlayer.js
+++ src/service/audio/trackPlayer.js
@@ -1,9 +1,9 @@
 import TrackPlayer from 'react-native-track-player';
 import store from '../../store/store';
-import { resetPlayer } from '../../actions/audio';
-import { incCurrentFeedIndex } from '../../actions/feed';
+import {resetPlayer} from '../../actions/audio';
+import {incCurrentFeedIndex} from '../../actions/feed';
 
-module.exports = async function() {
+module.exports = async function () {
   TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
 
   TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
@@ -12,13 +12,5 @@ module.exports = async function() {
 
   TrackPlayer.addEventListener('remote-seek', () => TrackPlayer.seekTo());
 
-  TrackPlayer.addEventListener('playback-track-changed', async (data) => {
-
-  });
-
-  // TrackPlayer.addEventListener('playback-queue-ended', (data) => {
-  //   // tell redux to update index to the index of the next item in feed
-  //   store.dispatch(resetPlayer());
-  //   store.dispatch(incCurrentFeedIndex());
-  // });
-}
+  TrackPlayer.addEventListener('playback-track-changed', async (data) => {});
+};
