64af29c6284004deeeaf2419e87f16e27da2986c
Maidul Islam <maidul98@gmail.com>
2021-02-19 23:40:49 -0500

    ask for permission

--- App.js
+++ App.js
@@ -6,14 +6,14 @@
  * @flow strict-local
  */
 
-import React, { useState, useEffect } from 'react';
-import { Provider } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {Provider} from 'react-redux';
 import store from './src/store/store';
 import 'react-native-gesture-handler';
 import moment from 'moment';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RegisterScreen from './src/components/screens/RegisterScreen';
@@ -25,27 +25,30 @@ import UserScreen from './src/components/screens/UserScreen';
 import SplashScreen from './src/components/screens/SplashScreen';
 import ProfileScreen from './src/components/screens/ProfileScreen';
 import NotificationsScreen from './src/components/screens/NotificationsScreen';
-import { getUser } from './src/service/api/users';
-import { handleFCMToken } from './src/service/notifications/notifications';
+import {getUser} from './src/service/api/users';
+import {
+  handleFCMToken,
+  requestUserPermission,
+} from './src/service/notifications/notifications';
 
 moment.updateLocale('en', {
   relativeTime: {
-    future : 'in %s',
-    past   : '%s ago',
-    s  : function (number, withoutSuffix) {
+    future: 'in %s',
+    past: '%s ago',
+    s: function (number, withoutSuffix) {
       return withoutSuffix ? 'now' : 'a few seconds';
     },
-    m  : '1m',
-    mm : '%dm',
-    h  : '1h',
-    hh : '%dh',
-    d  : '1d',
-    dd : '%dd',
-    M  : '1mth',
-    MM : '%dmth',
-    y  : '1y',
-    yy : '%dy'
-  }
+    m: '1m',
+    mm: '%dm',
+    h: '1h',
+    hh: '%dh',
+    d: '1d',
+    dd: '%dd',
+    M: '1mth',
+    MM: '%dmth',
+    y: '1y',
+    yy: '%dy',
+  },
 });
 
 const AuthStack = createStackNavigator();
@@ -55,79 +58,62 @@ const Tab = createBottomTabNavigator();
 // Insert Profile Screen onto this stack
 const FeedStackScreen = () => {
   return (
-    <FeedStack.Navigator
-      screenOptions={{ headerShown: false }}
-    >
-      <FeedStack.Screen 
-        name="Feed"
-        component={FeedScreen}
-      />
-      <FeedStack.Screen 
-        name="Profile"
-        component={ProfileScreen}
-      />
-      <FeedStack.Screen 
-        name="Notifications"
-        component={NotificationsScreen}
-      />
+    <FeedStack.Navigator screenOptions={{headerShown: false}}>
+      <FeedStack.Screen name="Feed" component={FeedScreen} />
+      <FeedStack.Screen name="Profile" component={ProfileScreen} />
+      <FeedStack.Screen name="Notifications" component={NotificationsScreen} />
     </FeedStack.Navigator>
   );
-}
+};
 
 const HomeTabScreen = () => {
-  return(
+  return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
           let iconName;
           switch (route.name) {
-            case "Feed":
+            case 'Feed':
               iconName = focused ? 'planet' : 'planet-outline';
               break;
-            case "Upload":
+            case 'Upload':
               iconName = focused ? 'mic' : 'mic-outline';
               break;
-            case "User":
+            case 'User':
               iconName = focused ? 'person-circle' : 'person-circle-outline';
               break;
-          } 
-  
+          }
+
           return <Icon name={iconName} size={size} color={color} />;
-        }
+        },
       })}
       tabBarOptions={{
         activeTintColor: 'rgb(52,152,219)',
         inactiveTintColor: 'rgb(127,140,141)',
-        showLabel: false
+        showLabel: false,
       }}
-      backBehavior="none"
-    >
-      <Tab.Screen 
-        name="Feed" 
-        component={FeedStackScreen} 
-      />
-      <Tab.Screen 
-        name="Upload" 
-        component={UploadScreen} 
-      />
-      <Tab.Screen 
-        name="User" 
-        component={UserScreen} 
-      />
+      backBehavior="none">
+      <Tab.Screen name="Feed" component={FeedStackScreen} />
+      <Tab.Screen name="Upload" component={UploadScreen} />
+      <Tab.Screen name="User" component={UserScreen} />
     </Tab.Navigator>
   );
-} 
+};
 
 const App: () => React$Node = () => {
   const [validToken, setValidToken] = useState(null);
 
   useEffect(() => {
     const bootstrapAsync = async () => {
-      let userToken; 
+      await handleFCMToken();
+      await requestUserPermission();
+
+      let userToken;
       try {
         // case: try restore token from AsyncStorage
         userToken = await AsyncStorage.getItem('userToken');
-        getUser(userToken,
+        getUser(
+          userToken,
           () => {
             // case: valid token
             setValidToken(true);
@@ -135,12 +121,13 @@ const App: () => React$Node = () => {
           () => {
             // case: invalid token
             setValidToken(false);
-          });
+          },
+        );
       } catch (e) {
         // case: restore token failed
         setValidToken(false);
-      }      
-    }
+      }
+    };
     bootstrapAsync();
   });
 
@@ -148,52 +135,28 @@ const App: () => React$Node = () => {
     switch (validToken) {
       case null:
         // render Splash screen
-        return (
-          <AuthStack.Screen 
-            name="Splash"
-            component={SplashScreen}
-          />
-        );
+        return <AuthStack.Screen name="Splash" component={SplashScreen} />;
       case false:
         // render Login screen
         return (
           <>
-            <AuthStack.Screen 
-              name="Welcome"
-              component={WelcomeScreen}
-            />
-            <AuthStack.Screen 
-              name="Login" 
-              component={LoginScreen} 
-            />
-            <AuthStack.Screen 
-              name="Register" 
-              component={RegisterScreen}
-            />
-            <AuthStack.Screen
-              name="Main"
-              component={HomeTabScreen}
-            />
+            <AuthStack.Screen name="Welcome" component={WelcomeScreen} />
+            <AuthStack.Screen name="Login" component={LoginScreen} />
+            <AuthStack.Screen name="Register" component={RegisterScreen} />
+            <AuthStack.Screen name="Main" component={HomeTabScreen} />
           </>
         );
       case true:
         // render Main screen
-        return (
-          <AuthStack.Screen
-            name="Main"
-            component={HomeTabScreen}
-          />
-        );
+        return <AuthStack.Screen name="Main" component={HomeTabScreen} />;
     }
-  }
+  };
 
   return (
     <>
       <Provider store={store}>
         <NavigationContainer>
-          <AuthStack.Navigator 
-            screenOptions={{ headerShown: false }}
-          >
+          <AuthStack.Navigator screenOptions={{headerShown: false}}>
             {renderScreen(validToken)}
           </AuthStack.Navigator>
         </NavigationContainer>
--- src/service/notifications/notifications.js
+++ src/service/notifications/notifications.js
@@ -1,10 +1,16 @@
 import iid from '@react-native-firebase/iid';
-import { registerDeviceToken } from '../api/notifications';
+import {registerDeviceToken} from '../api/notifications';
+import messaging from '@react-native-firebase/messaging';
+
+const requestUserPermission = async () => {
+  console.log('heheh');
+  const permission = await messaging().requestPermission(); // ask user for permission
+  console.log(permission);
+};
 
 const handleFCMToken = async () => {
-  // const id = await iid().get();
   const fcmToken = await iid().getToken();
   registerDeviceToken(fcmToken);
-}
+};
 
-export { handleFCMToken };
+export {handleFCMToken, requestUserPermission};
