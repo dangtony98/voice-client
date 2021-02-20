bc7e2cb341262c03b8259f82570cf60d71c1012a
Maidul Islam <maidul98@gmail.com>
2021-02-20 16:03:52 -0500

    changed to prod

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,5 +1,7 @@
 const DEV = false;
 
-const URL = DEV ? 'http://127.0.0.1:3000' : 'https://powerful-retreat-02063.herokuapp.com';
+const URL = DEV
+  ? 'http://127.0.0.1:3000'
+  : 'https://powerful-retreat-02063.herokuapp.com';
 
-export { URL }
+export {URL};
